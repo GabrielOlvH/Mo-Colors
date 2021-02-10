257dabad02343ff9a071e0bccf4394a8650ffdf3
StupPlayer <gabrielh.oliveira222@gmail.com>
2021-02-10 20:24:14 -0300

    fix glass pane being weird when coloring

--- src/main/java/me/steven/mocolors/mixin/MixinBlock.java
+++ src/main/java/me/steven/mocolors/mixin/MixinBlock.java
@@ -9,6 +9,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 
@@ -20,18 +21,24 @@ public class MixinBlock implements ColoredBlock {
         BlockState blockState = world.getBlockState(pos);
         Block block = blockState.getBlock();
         BlockEntity blockEntity = world.getBlockEntity(pos);
-        if (blockEntity instanceof ColoredBlockEntity) {}
-        else if (ConvertableBlocks.WOOL_BLOCKS.contains(block)) {
-            world.setBlockState(pos, MoColors.COLORED_WOOL.getDefaultState());
-        } else if (ConvertableBlocks.CONCRETE_BLOCKS.contains(block)) {
-            world.setBlockState(pos, MoColors.COLORED_CONCRETE.getDefaultState());
-        } else if (block == Blocks.SLIME_BLOCK) {
-            world.setBlockState(pos, MoColors.COLORED_SLIME.getDefaultState());
-        } else if (ConvertableBlocks.GLASS_BLOCKS.contains(block)) {
-            world.setBlockState(pos, MoColors.COLORED_GLASS.getDefaultState());
-        } else if (ConvertableBlocks.GLASS_PANE_BLOCKS.contains(block)) {
-            world.setBlockState(pos, MoColors.COLORED_GLASS_PANE.getDefaultState());
-        } else return false;
+        if (!(blockEntity instanceof ColoredBlockEntity)) {
+            if (ConvertableBlocks.WOOL_BLOCKS.contains(block)) {
+                world.setBlockState(pos, MoColors.COLORED_WOOL.getDefaultState());
+            } else if (ConvertableBlocks.CONCRETE_BLOCKS.contains(block)) {
+                world.setBlockState(pos, MoColors.COLORED_CONCRETE.getDefaultState());
+            } else if (block == Blocks.SLIME_BLOCK) {
+                world.setBlockState(pos, MoColors.COLORED_SLIME.getDefaultState());
+            } else if (ConvertableBlocks.GLASS_BLOCKS.contains(block)) {
+                world.setBlockState(pos, MoColors.COLORED_GLASS.getDefaultState());
+            } else if (ConvertableBlocks.GLASS_PANE_BLOCKS.contains(block)) {
+                world.setBlockState(pos, MoColors.COLORED_GLASS_PANE.getDefaultState());
+                for (Direction direction : Direction.values()) {
+                    BlockState state = world.getBlockState(pos);
+                    BlockPos sidePos = pos.offset(direction);
+                    world.setBlockState(pos, state.getStateForNeighborUpdate(direction, world.getBlockState(sidePos), world, pos, sidePos));
+                }
+            } else return false;
+        }
         if (blockEntity == null) blockEntity = world.getBlockEntity(pos);
         ((ColoredBlockEntity)blockEntity).setColor(color);
         blockEntity.markDirty();
