d51823642fc5f00ff96fa588a7359c301383702f
GabrielOlvH <gabrielh.oliveira222@gmail.com>
2021-07-16 11:22:43 -0300

    fix missing texture and bump version

--- src/main/java/me/steven/mocolors/blocks/models/ColoredBasicModel.java
+++ src/main/java/me/steven/mocolors/blocks/models/ColoredBasicModel.java
@@ -1,13 +1,18 @@
 package me.steven.mocolors.blocks.models;
 
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.ModelBakeSettings;
 import net.minecraft.client.render.model.ModelLoader;
+import net.minecraft.client.render.model.UnbakedModel;
 import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.util.SpriteIdentifier;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
 import java.util.function.Function;
 
 public class ColoredBasicModel extends ColoredBakedModel {
@@ -31,6 +36,11 @@ public class ColoredBasicModel extends ColoredBakedModel {
     @Override
     public Sprite getSprite() {
         return sprite;
+    }
+
+    @Override
+    public Collection<SpriteIdentifier> getTextureDependencies(Function<Identifier, UnbakedModel> unbakedModelGetter, Set<Pair<String, String>> unresolvedTextureReferences) {
+        return Collections.singleton(spriteId);
     }
 
     @Nullable
